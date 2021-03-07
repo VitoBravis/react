b7bf7586d85bc6bcc983f035b7a33733833a2864
Andrei <vitobravis@gmail.com>
2021-03-07 17:47:21 +0700

    first commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,18 @@
+import '../App.css';
+import '../style/test.scss'
+import styled from 'styled-components'
+
+const Main = styled.div`
+    background: aqua;
+    font-size: 100px;
+`
+
+const Index = () => {
+    return (
+        <Main>
+            Main
+        </Main>
+    );
+}
+
+export default Index;
